b58ed83a411a43c7ab9e298af61abd503b32cb09
marshift <marshift@riseup.net>
2024-03-18 02:57:56 +0000

    [Pages > Blog] RSS

---
+++ src/pages/blog/feed.xml.ts
@@ -0,0 +1,10 @@
+import type { APIRoute } from "astro";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+
+export const GET: APIRoute = async (context) => rss({
+    title: "the swamp",
+    description: "marsh's blog - excerpts of my mind, to be read at your leisure.",
+    site: context.site + "/blog",
+    // @ts-expect-error oh god
+    items: await pagesGlobToRssItems(import.meta.glob("./**/*.{md,mdx}")),
+});
