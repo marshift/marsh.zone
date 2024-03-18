74d91540b7009fb55d1a2661b5ad71a016d53213
marshift <marshift@riseup.net>
2024-03-18 03:16:41 +0000

    [Pages > Blog] Extra RSS goodies

--- src/pages/blog/feed.xml.ts
+++ src/pages/blog/feed.xml.ts
@@ -4,7 +4,8 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 export const GET: APIRoute = async (context) => rss({
     title: "the swamp",
     description: "marsh's blog - excerpts of my mind, to be read at your leisure.",
-    site: context.site + "/blog",
+    site: context.site + "blog",
+    stylesheet: "/rss/styles.xsl",
     // @ts-expect-error oh god
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.{md,mdx}")),
 });
