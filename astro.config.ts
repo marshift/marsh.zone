1523b218280da4fd5fc9cdedf392ecb1a89271f5
marshift <marshift@riseup.net>
2024-06-19 23:41:10 +0100

    [Meta] Add Astro's SolidJS integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://marsh.zone/",
-    integrations: [mdx()],
+    integrations: [solid(), mdx()],
     markdown: {
         shikiConfig: {
-            theme: "catppuccin-macchiato"
-        }
+            theme: "catppuccin-macchiato",
+        },
     },
     devToolbar: {
         enabled: false,
