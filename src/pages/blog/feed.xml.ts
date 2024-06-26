74da1147384919a56fe0cafad7785de9b649001a
marshift <marshift@riseup.net>
2024-06-27 00:20:20 +0100

    [Blog > RSS] Fix odd broken builds

--- src/pages/blog/feed.xml.ts
+++ src/pages/blog/feed.xml.ts
@@ -1,11 +1,10 @@
 import type { APIRoute } from "astro";
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss, { pagesGlobToRssItems, type RSSFeedItem } from "@astrojs/rss";
 
 export const GET: APIRoute = async (context) => rss({
     title: "the swamp",
     description: "marsh's blog - excerpts of my mind, to be read at your leisure.",
     site: context.site + "blog",
     stylesheet: "/rss/styles.xsl",
-    // @ts-expect-error oh god
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.{md,mdx}")),
+    items: await pagesGlobToRssItems(import.meta.glob("./**/*.{md,mdx}")) as RSSFeedItem[],
 });
