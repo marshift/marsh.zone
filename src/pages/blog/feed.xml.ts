5706ddc8cb36014d8c3152330773661921d2dcc7
marshift <marshift@riseup.net>
2024-12-31 19:33:40 +0000

    meta: update astro and other dependencies

--- src/pages/blog/feed.xml.ts
+++ src/pages/blog/feed.xml.ts
@@ -1,10 +1,10 @@
 import type { APIRoute } from "astro";
-import rss, { pagesGlobToRssItems, type RSSFeedItem } from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export const GET: APIRoute = async (context) => rss({
     title: "the swamp",
     description: "marsh's blog - excerpts of my mind, to be read at your leisure.",
     site: context.site + "blog",
     stylesheet: "/rss/styles.xsl",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.{md,mdx}")) as RSSFeedItem[],
+    items: await pagesGlobToRssItems(import.meta.glob("./**/*.{md,mdx}")),
 });
